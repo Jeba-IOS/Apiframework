eef8510d672da247aa148814da87c73f3c09e760
Bareeth <B>
2024-04-30 17:54:24 +0530

    initial setup  -- JEBA

---
+++ APIcallcustomFramework/APIcallcustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  APIcallcustomFramework.h
+//  APIcallcustomFramework
+//
+//  Created by Bareeth on 30/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for APIcallcustomFramework.
+FOUNDATION_EXPORT double APIcallcustomFrameworkVersionNumber;
+
+//! Project version string for APIcallcustomFramework.
+FOUNDATION_EXPORT const unsigned char APIcallcustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIcallcustomFramework/PublicHeader.h>
+
+
